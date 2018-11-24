04db17ee3ef542df54ea9b83239479c6d8cbc842
JanReichelt <45282277+JanReichelt@users.noreply.github.com>
2018-11-24 12:34:08 +0100

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,8 @@
+	console.log('haus');
+function clear() {
+	console.log('haus');
+	// var canvas1 = document.getElementsByTagName('CANVAS');
+	// for(var i = 0; i < canvas1.length; i++) {
+		// console.log(canvas1);
+		// }
+}
