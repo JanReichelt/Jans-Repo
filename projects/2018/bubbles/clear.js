de1bf704c6e28e8b21a9e43f02839a15fc1df71c
JanReichelt <janreichelt85@gmail.com>
2019-04-12 00:07:22 +0200

    changed bubbles to standalone

--- projects/2018/bubbles/clear.js
+++
@@ -1,7 +0,0 @@
-function cCanvas() {
-	var canvaslist = document.getElementsByTagName('CANVAS');
-	var body = document.getElementsByTagName('BODY')[0]; 
-	for(var i = 0; i < canvaslist.length; i++) {
-		body.removeChild(canvaslist[i]);
-	}
-}
